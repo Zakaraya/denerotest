0f8f07fb68732406015e60b34dc4435c58d32c9b
Boris Zakarya <44663025+Zakaraya@users.noreply.github.com>
2021-05-18 09:50:41 +0300

    Create task3.js
    complete

---
+++ task3.js
@@ -0,0 +1,16 @@
+  $(document).keydown(function(e) {
+    switch (e.which) {
+      case 37:
+        alert ("left arrow"); 
+        break;
+      case 38:
+      	alert ("up arrow"); 
+      	break;    
+      case 39:
+      	alert ("right arrow"); 
+      	break;
+      case 40:
+        alert ("down arrow"); 
+        break;
+    }
+  })
